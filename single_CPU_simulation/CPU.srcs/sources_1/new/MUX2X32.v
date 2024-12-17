0fbe7696a637a801751807635fc4653863b212b1
Losanctuary <1022692180@qq.com>
2024-12-17 18:02:55 +0800

    third

--- single_CPU_simulation/CPU.srcs/sources_1/new/MUX2X32.v
+++
@@ -1,7 +0,0 @@
-//32位2选1数据选择器
-module MUX2X32(A1,A2,S,Y);
-    input [31:0]A1,A2;
-    input S;
-    output [31:0]Y;
-    assign Y = (S == 1'b1) ? A2 : A1;
-endmodule
--- single_CPU_simulation/CPU.srcs/sources_1/new/SignExtend.v
+++
@@ -1,9 +0,0 @@
-module SignExtend(X,Se,Y);
-    input [15:0]X;
-    input Se;
-    output [31:0]Y;
-    wire [31:0]E0,E1;
-    assign E0={16'b0000000000000000, X};
-    assign E1={{16{X[15]}}, X};
-    MUX2X32 i(E0,E1,Se,Y);
-endmodule
